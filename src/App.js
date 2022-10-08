52c749abcad723b22c32e26666de1ecb37c6745e
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2022-10-08 22:42:01 +0300

    added navigation bar

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import pages
+import Home from './pages/Home'
+// import About from './pages/About'
+// import SingleCocktail from './pages/SingleCocktail'
+
+//import component
+
+import Navbar from './components/Navbar'
+
+function App () {
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default App
