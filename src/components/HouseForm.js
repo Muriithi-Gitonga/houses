d2bfb733b224efbe5665b5d13ba6571d6dce7973
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2022-10-09 02:21:43 +0300

    added form

--- src/components/HouseForm.js
+++ src/components/HouseForm.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
-    return <h1>I am a form</h1>
+    const [form, setForm] = useState({"location":"", "category":"", "price":"", "owner-contact":"", "image": ""})
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    return (
+        <form className="new-task-form" onSubmit={handleSubmit}>
+        <label>
+          Location
+          <input
+            type="text"
+            name="text"
+            onChange={(e) =>
+              setForm((form) => ({ ...form, location: e.target.value }))
+            }
+          />
+        </label>
+        <label>
+          Category
+          <input
+            name="category"
+            onChange={(e) =>
+              setForm((form) => ({ ...form, category: e.target.value }))
+            }
+          />
+        </label>
+        <input type="submit" value="Add task" />
+      </form>
+    )
 }
 
 export default Form
