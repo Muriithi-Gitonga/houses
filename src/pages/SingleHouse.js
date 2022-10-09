bfeafd7cf7b1e1c3ca5dc714c7b6a0522f9f242f
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2022-10-09 10:13:01 +0300

    added some css

--- src/pages/SingleHouse.js
+++ src/pages/SingleHouse.js
@@ -8,26 +8,29 @@ const SingleHouse = () => {
     useEffect (() => {
         const fetchSingleHouse = async () =>{
             const data = await (await fetch(`http://localhost:8002/houses/${id}`)).json()
-            console.log(data)
-            if (data) {
-                const {location, category, price, contact,} = data
-                const newSingleHouse = {place:location, size:category, budget:price, contacts:contact }
-                setSingleHouse((singleHouse) => newSingleHouse)
-
-            } else {
-                setSingleHouse(null)
-            }
-
-        } 
+            const {location, category, price,image, contact,} = data
+            const newSingleHouse = {place:location, size:category, budget:price, contacts:contact, picture:image }
+            setSingleHouse((singleHouse) => newSingleHouse)
+        }
         fetchSingleHouse()
     }, [id])
 
-    const {place} = singleHouse
+    const {place,picture} = singleHouse
     
     return (
-        <section className="section cpcktail-section">
+        <section className="section house-section">
             <Link className="bt btn-primary" to='/'>back home</Link>
             <h2 className="section-title">{place}</h2>
+            <div className="house">
+                <img src={picture} alt='houseimage'/>
+                <div className="house-info">
+                <p>
+                    <span className="house-data">Location: </span>
+                    {place}
+                 </p>
+
+                </div>
+            </div>
         </section>
     )
 
