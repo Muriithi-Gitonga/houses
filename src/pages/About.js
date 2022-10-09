e1115b12be7d5f890cc601224d8dab4ce1185696
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2022-10-09 10:59:38 +0300

    modified about component

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <section className='section about-section'>
       <h1 className='section-title'>about us</h1>
       <p>
-        Browse the highest quality listings, call the owner, and even get your title deed online. the perfect section to get your home!
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 
       </p>
     </section>
